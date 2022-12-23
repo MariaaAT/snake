e3f7cb9609363af497bc94b918c8b990c119190b
MariaaAT <mat9296@gmail.com>
2022-12-23 09:51:46 +0100

    add random block

--- snake/__main__.py
+++ snake/__main__.py
@@ -1,3 +1,5 @@
+import random
+
 import pygame
 
 
@@ -11,8 +13,8 @@ width, height = 640, 480
 screen = pygame.display.set_mode((width, height))
 
 field_size = 20, 14
-block_w = width/field_size[0]
-block_h = height/field_size[1]
+block_w = width // field_size[0]
+block_h = height // field_size[1]
 
 # snake
 snake = [(3, 3), (2, 3), (2, 2), (2, 1)]
@@ -30,10 +32,15 @@ dir = (1, 0)
 running = True
 while running:
     # Moving the snake down
-    snake = [(snake[0][0] + dir[0], snake[0][1] + dir[1])] + snake[:-1]
+    snake = [((snake[0][0] + dir[0]) % field_size[1], (snake[0][1] + dir[1]) % field_size[0])] + snake[:-1]
 
     # Set the background color to white
     screen.fill((255, 255, 255))
+
+    # Add a block in random position
+    random_block_x = random.randint(0, field_size[0])
+    random_block_y = random.randint(0, field_size[1])
+    random_block = pygame.draw.rect(screen, color, pygame.Rect(random_block_y * block_h, random_block_x * block_w, block_h, block_w))
 
     # Draw the snake
     pygame.draw.rect(screen, color_head, pygame.Rect(snake[0][1] * block_h, snake[0][0] * block_w, block_h, block_w))
@@ -44,10 +51,10 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # Get the state of the keyboard keys
+    # Get the state of the keyboard keys
     keys = pygame.key.get_pressed()
 
-    # If the right arrow key is pressed, print "guacamole"
+    # Change direction if arrow keys pressed
     if keys[pygame.K_RIGHT] and dir != (0, -1):
         dir = (0, 1)
     elif keys[pygame.K_LEFT] and dir != (0, 1):
@@ -57,21 +64,8 @@ while running:
     elif keys[pygame.K_DOWN] and dir != (-1, 0):
         dir = (1, 0)
 
-    new_dir_xaxis = (dir[0], 0)
-    new_dir_yaxis = (0, dir[0])
-
-    # Make the snake appear from the other side of the screen
-
-    if snake[1][0] > width:
-        print("Stop")
-    if snake[1][1] > height:
-        print("Stop")
-
-        # Draw the snake at the new position
-
     # Update the window display
     pygame.display.flip()
-
 
     clock.tick(3)
 
