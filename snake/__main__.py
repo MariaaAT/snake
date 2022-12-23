3ed8b1d3a852761842461ed615b2092ad00c44a9
MariaaAT <mat9296@gmail.com>
2022-12-23 11:18:47 +0100

    fixed coordinates, fixed movement

--- snake/__main__.py
+++ snake/__main__.py
@@ -1,7 +1,5 @@
 import random
-
 import pygame
-
 
 # Initialize Pygame
 pygame.init()
@@ -27,27 +25,31 @@ color_head = (0, 255, 0)
 clock = pygame.time.Clock()
 
 dir = (1, 0)
+# Set the treat variable outside the loop so the timer doesn't affect this variable
+treat = (random.randint(0, field_size[0] - 1), random.randint(0, field_size[1] - 1))
+
+MOVE_EVENT = pygame.USEREVENT + 1
+pygame.event.post(pygame.event.Event(MOVE_EVENT))
 
 # Run the Pygame loop
 running = True
 while running:
-    # Moving the snake down
-    snake = [((snake[0][0] + dir[0]) % field_size[1], (snake[0][1] + dir[1]) % field_size[0])] + snake[:-1]
-
-    # Set the background color to white
-    screen.fill((255, 255, 255))
-
-    # Add a block in random position
-    random_block_x = random.randint(0, field_size[0])
-    random_block_y = random.randint(0, field_size[1])
-    random_block = pygame.draw.rect(screen, color, pygame.Rect(random_block_y * block_h, random_block_x * block_w, block_h, block_w))
-
-    # Draw the snake
-    pygame.draw.rect(screen, color_head, pygame.Rect(snake[0][1] * block_h, snake[0][0] * block_w, block_h, block_w))
-    for block in snake[1:]:
-        pygame.draw.rect(screen, color, pygame.Rect(block[1] * block_h, block[0] * block_w, block_h, block_w))
-
     for event in pygame.event.get():
+        if event.type == MOVE_EVENT:
+            # Remember snake end
+            snake_end = snake[-1]
+
+            # Moving the snake down
+            snake = [((snake[0][0] + dir[0]) % field_size[0], (snake[0][1] + dir[1]) % field_size[1])] + snake[:-1]
+
+            # Check if snake ate the treat
+            if snake[0] == treat:
+                snake.append(snake_end)
+                treat = (random.randint(0, field_size[0] - 1), random.randint(0, field_size[1] - 1))
+                print(treat)
+
+            pygame.time.set_timer(pygame.event.Event(MOVE_EVENT), 250)
+
         if event.type == pygame.QUIT:
             running = False
 
@@ -55,19 +57,31 @@ while running:
     keys = pygame.key.get_pressed()
 
     # Change direction if arrow keys pressed
-    if keys[pygame.K_RIGHT] and dir != (0, -1):
+    if keys[pygame.K_DOWN] and dir != (0, -1):
         dir = (0, 1)
-    elif keys[pygame.K_LEFT] and dir != (0, 1):
+    elif keys[pygame.K_UP] and dir != (0, 1):
         dir = (0, -1)
-    elif keys[pygame.K_UP] and dir != (1, 0):
+    elif keys[pygame.K_LEFT] and dir != (1, 0):
         dir = (-1, 0)
-    elif keys[pygame.K_DOWN] and dir != (-1, 0):
+    elif keys[pygame.K_RIGHT] and dir != (-1, 0):
         dir = (1, 0)
+
+    # Set the background color to white
+    screen.fill((255, 255, 255))
+
+    # Add a block in random position (treat)
+    pygame.draw.rect(screen, color, pygame.Rect(treat[0] * block_w, treat[1] * block_h, block_w, block_h))
+
+    # Draw the snake
+    pygame.draw.rect(screen, color_head,
+                     pygame.Rect(snake[0][0] * block_w, snake[0][1] * block_h, block_w, block_h))
+    for block in snake[1:]:
+        pygame.draw.rect(screen, color, pygame.Rect(block[0] * block_w, block[1] * block_h, block_w, block_h))
 
     # Update the window display
     pygame.display.flip()
 
-    clock.tick(3)
+    clock.tick(60)
 
 # Quit Pygame
 pygame.quit()
