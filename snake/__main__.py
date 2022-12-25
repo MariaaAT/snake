a7fd6e258338093352f34cc8375492ae318fb4a4
MariaaAT <mat9296@gmail.com>
2022-12-25 16:34:10 +0100

    game over

--- snake/__main__.py
+++ snake/__main__.py
@@ -1,6 +1,5 @@
 import random
 import pygame
-from pygame.locals import *
 
 # Initialize Pygame
 pygame.init()
@@ -15,7 +14,7 @@ field_size = 20, 14
 block_w = width // field_size[0]
 block_h = height // field_size[1]
 myfont = pygame.font.SysFont("cochin", 16)
-print(pygame.font.get_fonts())
+go_font = pygame.font.SysFont("cochin", 25)
 
 # snake
 snake = [(3, 3), (2, 3), (2, 2), (2, 1)]
@@ -35,14 +34,16 @@ MOVE_EVENT = pygame.USEREVENT + 1
 pygame.event.post(pygame.event.Event(MOVE_EVENT))
 
 # Set the score
-
 score = 0
+
+# Set collision
+dead = False
 
 # Run the Pygame loop
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == MOVE_EVENT:
+        if event.type == MOVE_EVENT and not dead:
             # Remember snake end
             snake_end = snake[-1]
 
@@ -54,6 +55,11 @@ while running:
                 snake.append(snake_end)
                 treat = (random.randint(0, field_size[0] - 1), random.randint(0, field_size[1] - 1))
                 score += 1
+
+            # Todo check collision with itself
+            if snake[0] in snake[1:]:
+                dead = True
+                pygame.time.set_timer(pygame.event.Event(pygame.QUIT), 5000)
 
             pygame.time.set_timer(pygame.event.Event(MOVE_EVENT), 250)
 
@@ -85,8 +91,17 @@ while running:
     for block in snake[1:]:
         pygame.draw.rect(screen, color, pygame.Rect(block[0] * block_w, block[1] * block_h, block_w, block_h))
 
+    # Update score
+    # You need  to set a font (myfont)
+    # render() allows to add a new surface on top of that surface
+    # screen.blit() carries out the appearance of that text
     scoretext = myfont.render(f"Score: {score}", False, (0, 0, 0))
     screen.blit(scoretext, (5, 10))
+
+    # Game over
+    if dead:
+        game_over_text = go_font.render("GAME OVER!", False, (0, 0, 0))
+        screen.blit(game_over_text, [(width / 2) - 100, height / 2])
 
     # Update the window display
     pygame.display.flip()
