e626702bede1ce623879ebaa2aee93e4c446e0a5
MariaaAT <mat9296@gmail.com>
2022-12-27 17:41:33 +0100

    button class created

---
+++ snake/button.py
@@ -0,0 +1,34 @@
+import pygame.mouse
+
+
+class Button():
+    # button instance created
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()  # get a rectangle from it
+        self.rect.topleft = (x, y)
+        self.clicked = False
+
+    # Draw the button on the screen
+    def draw(self, surface):
+        # allow different buttons do different things
+        action = False
+
+        # get mouse position
+        pos = pygame.mouse.get_pos()
+
+        # check mouseover and clicked conditions
+        if self.rect.collidepoint(pos):
+            # [] the number inside the bracket means the button we pressed on the mouse.
+            # 0 is for left, 2 is for right
+            # We equal that to 1 meaning that we pressed it
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        surface.blit(self.image, self.rect.x, self.rect.y)
+
+        return action
